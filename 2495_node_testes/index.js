9a2557b321243607ce0f302d1308b6eb919bc4f4
Filipe Muliterno <filipemuliternop@hotmail.com>
2026-02-26 17:47:35 -0300

    finalizando trabalho

--- 2495_node_testes/index.js
+++ 2495_node_testes/index.js
@@ -1,15 +1,36 @@
-const somaHorasExtras = (salario, valorHorasExtras) => salario + valorHorasExtras;
+const somaHorasExtras = (salario, valorHorasExtras) =>
+  salario + valorHorasExtras;
 
 const calculaDescontos = (salario, descontos) => salario - descontos;
 
-const teste = (titulo, esperado, retornado) => {
-  if (esperado === retornado) {
-    console.log(`${titulo} passou`);
-  } else {
-    console.error(`${titulo} não passou`);
+const verifiqueSe = (valor) => {
+  const assercoes = {
+    ehExatamenteIgualA(esperado) {
+      if (valor !== esperado) {
+        throw {};
+      }
+    },
+  };
+  return assercoes;
+};
+
+const teste = (titulo, funcaoDeTeste) => {
+  try {
+    funcaoDeTeste();
+    console.log(`${titulo} passou nos testes`);
+  } catch {
+    console.error(`${titulo} não passou nos testes`);
   }
 };
 
-teste('somaHorasExtras', 2500, somaHorasExtras(2000, 500));
+teste("somaHorasExtras", () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 500);
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
 
-teste('calculaDesconto', 2200, calculaDescontos(2500, 300));
+teste("calculaDesconto", () => {
+  const esperado = 2500;
+  const retornado = somaHorasExtras(2000, 300);
+  verifiqueSe(retornado).ehExatamenteIgualA(esperado);
+});
