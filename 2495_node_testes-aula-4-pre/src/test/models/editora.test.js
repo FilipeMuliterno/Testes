206f339f2257a6417c2207a265b25c5acd9ef83d
Filipe Muliterno <filipemuliternop@hotmail.com>
2026-03-02 17:50:15 -0300

    .

---
+++ 2495_node_testes-aula-4-pre/src/test/models/editora.test.js
@@ -0,0 +1,58 @@
+import { describe, expect, jest } from "@jest/globals";
+import Editora from "../../models/editora.js";
+
+describe("Testando o modelo Editora", () => {
+  const objetoEditora = {
+    nome: "Teste",
+    cidade: "Lagoa Vermelha",
+    email: "teste@teste.com",
+  };
+
+  it("Deve instanciar uma nova editora", () => {
+    const editora = new Editora(objetoEditora);
+    expect(editora).toEqual(expect.objectContaining(objetoEditora));
+  });
+
+  it.skip("Deve salvar editora no BD", () => {
+    const editora = new Editora(objetoEditora);
+    editora.salvar().then((dados) => {
+      expect(dados.nome).toBe(objetoEditora.nome);
+    });
+  });
+
+  it.skip("Deve salvar editora no BD usando a sintaxe moderna", async () => {
+    const editora = new Editora(objetoEditora);
+    const dados = await editora.salvar();
+    const comparar = await Editora.pegarPeloId(dados.id);
+    expect(comparar).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      })
+    );
+  });
+
+  it("Deve fazer uma chamada simulada ao BD", () => {
+    const editora = new Editora(objetoEditora);
+    editora.salvar = jest.fn().mockReturnValue({
+      id: 10,
+      nome: "Teste",
+      cidade: "Lagoa Vermelha",
+      email: "teste@teste.com",
+      created_at: "2022-10-01",
+      updated_at: "2022-10-01",
+    });
+    const retorno = editora.salvar();
+
+    expect(retorno).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      })
+    );
+  });
+});
---
+++ 2495_node_testes-aula-4/src/test/routes/editorasRoutes.test.js
@@ -0,0 +1,37 @@
+import app from "../../app.js";
+import request from "supertest";
+
+let server;
+beforeEach(() => {
+  const port = 3000;
+  server = app.listen(port);
+});
+
+afterEach(() => {
+  server.close();
+});
+
+describe("GET em /editoras", () => {
+  it("Deve retornar uma lista de editoras", async () => {
+    const resposta = await request(app)
+      .get("/editoras")
+      .set("Accept", "application/json")
+      .expect("content-type", /json/)
+      .expect(200);
+
+    expect(resposta.body[0].email).toEqual("e@e.com");
+  });
+});
+
+describe("POST em /editorar", () => {
+  it("Deve salvar uma nova editora", async () => {
+    await request(app)
+      .post("/editoras")
+      .send({
+        nome: "CDC",
+        cidade: "Sao Paulo",
+        email: "c@c.com",
+      })
+      .expect(201);
+  });
+});
